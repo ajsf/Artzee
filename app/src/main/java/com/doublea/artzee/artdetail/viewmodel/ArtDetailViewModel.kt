59f7f6d45e3c7cf1b06204748366c70a059abe8b
Aaron Friedman <aaron.js.friedman@gmail.com>
2019-02-12 16:03:08 -1000

    Add repository to ViewModel constructors, with default implementations on ArtRepositoryImpl. Refactor ArtDetailViewModel to not expose MutableLiveData.

--- app/src/main/java/com/doublea/artzee/artdetail/viewmodel/ArtDetailViewModel.kt
+++ app/src/main/java/com/doublea/artzee/artdetail/viewmodel/ArtDetailViewModel.kt
@@ -2,41 +2,48 @@ package com.doublea.artzee.artdetail.viewmodel
 
 import android.util.Log
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.MediatorLiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.*
 import com.doublea.artzee.commons.data.models.Art
 import com.doublea.artzee.commons.data.models.Artist
+import com.doublea.artzee.commons.repository.ArtRepository
 import com.doublea.artzee.commons.repository.ArtRepositoryImpl
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.rxkotlin.subscribeBy
 
-class ArtDetailViewModel : ViewModel() {
+class ArtDetailViewModel(private val repository: ArtRepository = ArtRepositoryImpl()) : ViewModel() {
 
-    val artLiveData: MutableLiveData<Art> = MutableLiveData()
-    val artistLiveData: MediatorLiveData<Artist> = MediatorLiveData()
+    val artLiveData: LiveData<Art>
+        get() = _artLiveData
 
-    private val repository = ArtRepositoryImpl()
+    val artistLiveData: LiveData<Artist>
+        get() = _artistLiveData
+
+    private val _artLiveData: MutableLiveData<Art> = MutableLiveData()
+
+    private val _artistLiveData: MediatorLiveData<Artist> = MediatorLiveData()
 
     init {
-        artistLiveData.addSource(artLiveData) { artEvent ->
+        _artistLiveData.addSource(artLiveData) { artEvent ->
             artEvent?.let { art ->
                 repository
                         .getArtistForArtwork(art.id)
                         .observeOn(AndroidSchedulers.mainThread())
                         .subscribeBy(
                                 { Log.e("ERROR", it.message) },
-                                { artistLiveData.value = it }
+                                { _artistLiveData.value = it }
                         )
             }
         }
     }
 
+    fun selectArt(art: Art) {
+        _artLiveData.value = art
+    }
+
     companion object {
         fun createViewModel(fragment: Fragment, art: Art): ArtDetailViewModel {
             val viewModel = ViewModelProviders.of(fragment).get(ArtDetailViewModel::class.java)
-            viewModel.artLiveData.value = art
+            viewModel.selectArt(art)
             return viewModel
         }
     }
--- app/src/main/java/com/doublea/artzee/browse/viewmodel/BrowseArtViewModel.kt
+++ app/src/main/java/com/doublea/artzee/browse/viewmodel/BrowseArtViewModel.kt
@@ -5,11 +5,10 @@ import androidx.lifecycle.ViewModel
 import androidx.paging.PagedList
 import com.doublea.artzee.commons.data.models.Art
 import com.doublea.artzee.commons.navigator.Navigator
+import com.doublea.artzee.commons.repository.ArtRepository
 import com.doublea.artzee.commons.repository.ArtRepositoryImpl
 
-class BrowseArtViewModel : ViewModel() {
-
-    private val repository = ArtRepositoryImpl()
+class BrowseArtViewModel(repository: ArtRepository = ArtRepositoryImpl()) : ViewModel() {
 
     val artList: LiveData<PagedList<Art>> = repository.getArtFeed()
 
