51d493386ecd1d8acbbdd048d0f5a5c35a8294ae
Aaron Friedman <aaron.js.friedman@gmail.com>
2019-02-13 22:22:45 -1000

    Create ArtzeeApp class and AppModule for DI.

---
+++ app/src/main/java/com/doublea/artzee/ArtzeeApp.kt
@@ -0,0 +1,13 @@
+package com.doublea.artzee
+
+import android.app.Application
+import com.doublea.artzee.commons.di.appModule
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+
+class ArtzeeApp : Application(), KodeinAware {
+
+    override val kodein: Kodein = Kodein.lazy {
+        import(appModule(this@ArtzeeApp))
+    }
+}
