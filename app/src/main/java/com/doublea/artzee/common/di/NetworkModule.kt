b81c9d9c7b825ad312c3d3ecec178bdb31bd5fb4
Aaron Friedman <aaron.js.friedman@gmail.com>
2019-02-14 02:23:01 -1000

    Update ArtApi to use ArtsyServiceToResponseMapper.

--- app/src/main/java/com/doublea/artzee/common/di/NetworkModule.kt
+++ app/src/main/java/com/doublea/artzee/common/di/NetworkModule.kt
@@ -1,11 +1,11 @@
 package com.doublea.artzee.common.di
 
-import com.doublea.artzee.common.mapper.ArtsyToArtMapper
+import com.doublea.artzee.common.mapper.ArtsyServiceToResponseMapper
 import com.doublea.artzee.common.mapper.ArtsyToArtistMapper
 import com.doublea.artzee.common.mapper.Mapper
-import com.doublea.artzee.common.model.Art
 import com.doublea.artzee.common.model.Artist
 import com.doublea.artzee.common.network.ArtApi
+import com.doublea.artzee.common.network.ArtApiResponse
 import com.doublea.artzee.common.network.ArtsyArtistsWrapper
 import com.doublea.artzee.common.network.ArtsyArtworkWrapper
 import com.doublea.artzee.common.network.retrofit.ArtsyService
@@ -18,6 +18,6 @@ import org.kodein.di.generic.singleton
 fun networkModule() = Kodein.Module("networkModule") {
     bind<ArtsyService>() with singleton { ArtsyService.getService() }
     bind<ArtApi>() with provider { ArtApi(instance(), instance(), instance()) }
-    bind<Mapper<ArtsyArtworkWrapper, List<Art>>>() with provider { ArtsyToArtMapper() }
+    bind<Mapper<ArtsyArtworkWrapper, ArtApiResponse>>() with provider { ArtsyServiceToResponseMapper() }
     bind<Mapper<ArtsyArtistsWrapper, Artist>>() with provider { ArtsyToArtistMapper() }
 }
--- app/src/main/java/com/doublea/artzee/common/network/ArtApi.kt
+++ app/src/main/java/com/doublea/artzee/common/network/ArtApi.kt
@@ -9,11 +9,12 @@ import io.reactivex.Single
 data class ArtApiResponse(val artList: List<Art>, val cursor: String)
 
 class ArtApi(private val service: ArtsyService,
-             private val artMapper: Mapper<ArtsyArtworkWrapper, List<Art>>,
+             private val artMapper: Mapper<ArtsyArtworkWrapper, ArtApiResponse>,
              private val artistMapper: Mapper<ArtsyArtistsWrapper, Artist>
 ) {
+
     fun getArt(cursor: String = ""): Single<ArtApiResponse> = callArtsyService(cursor)
-            .map { it.extractResponseData() }
+            .map { artMapper.toModel(it) }
 
     fun getArtistForArtwork(artworkId: String): Single<Artist> = service
             .getArtistsByArtworkId(artworkId)
@@ -23,15 +24,4 @@ class ArtApi(private val service: ArtsyService,
         return if (cursor.isNotBlank()) service.getArtByCursor(cursor) else service.getArt()
     }
 
-    private fun ArtsyArtworkWrapper.extractResponseData(): ArtApiResponse {
-        val artList = artMapper.toModel(this)
-        val cursor = getCursor(_links.next.href)
-        return ArtApiResponse(artList, cursor)
-    }
-
-    private fun getCursor(urlString: String): String {
-        val cursorIndex = urlString.indexOf("cursor=") + 7
-        val endCursorIndex = urlString.indexOf("&size=")
-        return urlString.slice(cursorIndex until endCursorIndex)
-    }
 }
