781e4d0769aeadbf5cd906c9f125410116f0ddf6
Aaron Friedman <aaron.js.friedman@gmail.com>
2019-02-13 22:34:10 -1000

    Create room database to cache art api responses, and ArtsyCache as class for app to interact with db.

---
+++ app/src/main/java/com/doublea/artzee/commons/db/room/ArtEntity.kt
@@ -0,0 +1,23 @@
+package com.doublea.artzee.commons.db.room
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "art")
+data class ArtEntity(
+        @PrimaryKey
+        val id: String,
+        val title: String,
+        val category: String,
+        val medium: String?,
+        val date: String?,
+        val collectingInstitution: String,
+        val image_versions: List<String>,
+        val thumbnail: String,
+        val image: String,
+        val partner: String,
+        val genes: String,
+        val artists: String,
+        val similarArtworks: String)
+
+
